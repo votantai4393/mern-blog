e888825385dcb395e444fefc909a30784d32e56e
votantaiH <22130244@st.hcmuaf.edu.vn>
2024-03-29 22:47:48 +0700

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+
 export default function App() {
-	return <div>App</div>
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
